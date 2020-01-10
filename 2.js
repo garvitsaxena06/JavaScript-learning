658aae288d34688cc1d7e4c165ba7eec7ff66e8e
garvitsaxena06 <garvitsaxena06.io>
2020-01-10 21:21:14 +0530

    second

---
+++ 2.js
@@ -0,0 +1,13 @@
+var a = 10, b = 5;
+//adding 2 numbers
+var sum = a + b;
+console.log(sum);
+//subtracting 2 numbers
+var diff = a - b;
+console.log(diff);
+//mutiplying 2 numbers
+var mul = a * b;
+console.log(mul);
+//dividing 2 numbers
+var div = a / b;
+console.log(div);
