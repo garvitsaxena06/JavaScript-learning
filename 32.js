e48158de7fe71883d897d1c8b0b249869d5aadb3
Garvit Saxena <garvitsaxena06@gmail.com>
2020-01-11 17:36:05 +0530

    Create 32.js

---
+++ 32.js
@@ -0,0 +1,8 @@
+//create strings using template literals
+const person = {
+    'name' : 'Garvit Saxena',
+    'age' : 19
+};
+
+const myStr = `Hello! My name is ${person.name} and I'm ${person.age} years old.`;
+console.log(myStr);
