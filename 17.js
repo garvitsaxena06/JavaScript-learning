44727f7a1bd29ecfdd9219eac960a43ba4786854
Garvit Saxena <garvitsaxena06@gmail.com>
2020-01-11 13:12:15 +0530

    Create 17.js

---
+++ 17.js
@@ -0,0 +1,6 @@
+//some math function
+var a = 5.23;
+var b = 2.34;
+console.log(Math.sqrt(a));                  //squareroot function
+console.log(Math.pow(a, b));                //power function                                
+console.log(Math.round(a));                 //round-off function
