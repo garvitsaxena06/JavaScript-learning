f640b42412575ba7149ef0eac0341bcd361ccf7f
Garvit Saxena <garvitsaxena06@gmail.com>
2020-01-11 09:27:56 +0530

    Create 5.js

---
+++ 5.js
@@ -0,0 +1,10 @@
+//to find first character of a string
+var myString = 'Hello World!';
+var firstChar = myString[0];
+console.log(firstChar);
+//to find nth character of a string
+var nChar = myString[4];
+console.log(nChar);
+//to find last character of a string
+var lastChar = myString[myString.length - 1];
+console.log(lastChar);
