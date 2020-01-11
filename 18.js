0eea1200909efb0efcccc0cb2b897dd0d43899c0
Garvit Saxena <garvitsaxena06@gmail.com>
2020-01-11 13:18:39 +0530

    Create 18.js

---
+++ 18.js
@@ -0,0 +1,13 @@
+//creating objects
+var myObj = {
+    'Name' : 'Garvit',
+    'Surname' : 'Saxena',
+    'College' : 'IIPS'
+};
+
+var var1 = myObj.Name;
+var var2 = myObj.Surname;
+var var3 = myObj.College;
+console.log(var1);
+console.log(var2);
+console.log(var3);
