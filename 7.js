78d07d18e158b5fa60437ea544b9508a3fa68cec
Garvit Saxena <garvitsaxena06@gmail.com>
2020-01-11 09:33:55 +0530

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,6 @@
+//modify data of an array with index
+var myArray = ['This', 'is', 'my', 'Array', 3, 6, 1, 2];
+console.log(myArray);
+myArray[4] = 'String';
+console.log('Modified array:');
+console.log(myArray);
